4b740695d2d8a6b334880722ae580672207bdbaa
Yuki Yamashita <163654391+Claus-ginger@users.noreply.github.com>
2026-03-07 21:44:08 +0900

    Add comparison logic in A.py
    Implement logic to compare elements of A with X and print results.

---
+++ ABC/ABC448/A.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+for i in range(N):
+  if A[i] < X:
+    X = A[i]
+    print("1")
+  else:
+    print("0")
