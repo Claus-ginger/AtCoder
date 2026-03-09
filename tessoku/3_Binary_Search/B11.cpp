74543d351425ad763f7423085240bea723631834
Yuki Yamashita <163654391+Claus-ginger@users.noreply.github.com>
2026-03-09 10:59:12 +0900

    Add binary search implementation in B11.cpp

---
+++ tessoku/3_Binary_Search/B11.cpp
@@ -0,0 +1,21 @@
+# include <iostream> //標準入出力を使用する
+# include <algorithm> //sortなどを使用する
+using namespace std;
+
+int N, A[100009];
+int Q, X[100009];
+
+int main(){
+  cin >> N;
+  for (int i = 1; i <= N; i++) cin >> A[i];
+  cin >> Q;
+  for (int i = 1; i <= Q; i++) cin >> X[i];
+  
+  sort(A + 1, A + N + 1);
+  
+  for (int i = 1; i <= Q; i++){
+    int pos1 = lower_bound(A + 1, A + N + 1, X[i]) - A;
+    cout << pos1 - 1 << endl;
+  }
+  return 0;
+}
