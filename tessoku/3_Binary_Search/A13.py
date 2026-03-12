cf3b188ace420ec00879ab7b7450c461f2e3e62d
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-03-12 13:33:02 +0900

    add A13.py

---
+++ tessoku/3_Binary_Search/A13.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+R = [None]*N
+#print(R)
+
+for i in range(0, N-1):
+    if i == 0: R[i] = 0
+    else: R[i] = R[i - 1]
+
+    while R[i] < N-1 and A[R[i]+1] - A[i] <= K:
+        R[i] += 1
+    
+ans = 0
+for i in range(0, N-1):
+    ans += R[i] - i
+
+print(ans)
