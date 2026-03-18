0bbe3cadfc09a2cb8cc813961e8f7ba2efc847a0
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-03-19 00:02:54 +0900

    change B18.py

--- tessoku/4_Dynamic_Programming/B18.py
+++ tessoku/4_Dynamic_Programming/B18.py
@@ -10,7 +10,7 @@ for i in range(1, S+1):
     dp[0][i] = False
 
 for i in range(1, N+1):
-    for j in range(0, S-1):
+    for j in range(0, S+1):
         if j < A[i-1]:
             if dp[i-1][j] == True:
                 dp[i][j] = True
