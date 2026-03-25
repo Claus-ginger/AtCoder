0e0aeaac0aea922a1d7ec3731ea3841c6ffce709
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-03-26 00:50:23 +0900

    update B20.py

--- tessoku/4_Dynamic_Programming/B20.py
+++ tessoku/4_Dynamic_Programming/B20.py
@@ -3,3 +3,19 @@ input = sys.stdin.readline
 
 S = input()
 T = input()
+
+dp = [[None]*(len(T)+1) for i in range(len(S)+1)]
+
+dp[0][0] = 0
+for i in range(len(S)+1):
+    for j in range(len(T)+1):
+        if i>=1 and j>=1 and S[i-1] == T[j-1]:
+            dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1])
+        elif i >= 1 and j >= 1:
+            dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+        elif i >= 1:
+            dp[i][j] = dp[i-1][j] + 1
+        elif j >= 1:
+            dp[i][j] = dp[i][j-1] + 1
+
+print(dp[len(S)][len(T)])
