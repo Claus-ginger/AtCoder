5eecdba8bfb475f8bb434495a84834af3c77dee7
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-03-31 23:22:56 +0900

    update C.py

--- ABC/ABC501/C.py
+++ ABC/ABC501/C.py
@@ -11,7 +11,7 @@ for _ in range(Q):
     if n == 1:
         heapq.heappush(T, h)
     else:
-        while T and T[0] <= h:
+        while T and T[0] <= h: # T を入れている理由：Tが空だとT[0]が存在しないのでIndexErrorになる．andで評価して，Falseなら右は評価しない
             heapq.heappop(T)
 
     print(len(T))
