1b8f81faa29fbd05c1a3fb7e94a7e520986cc8b4
Yuki Yamashita <163654391+Claus-ginger@users.noreply.github.com>
2026-04-04 13:56:46 +0900

    add A24.py

---
+++ tessoku/4_Dynamic_Programming/A24.py
@@ -0,0 +1,23 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+LEN = 0
+L = []
+dp = [None] * N
+
+for i in range(N):
+    pos = bisect.bisect_left(L, A[i])
+    dp[i] = pos
+
+    if dp[i] >= LEN:
+        L.append(A[i])
+        LEN += 1
+    else:
+        L[dp[i]] = A[i]
+        
+
+print(LEN)
