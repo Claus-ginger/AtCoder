494f80a13af6756f861402400335a239fc348d97
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-04-05 12:48:15 +0900

    update A.py

--- ABC/ABC502/A.py
+++ ABC/ABC502/A.py
@@ -3,3 +3,10 @@ if (M == 1 and D == 7) or (M == 3 and D == 3) or (M == 5 and D == 5) or (M == 7 
   print("Yes")
 else:
   print("No")
+
+# 解説
+# gissekulist = [(1, 7), (3, 3), (5, 5), (7, 7), (9 9)]
+# if ((m, d) in gossekulist):
+#     print("Yes")
+# else:
+#     print("No")   
