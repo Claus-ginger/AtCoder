125a059fa69355e89f6ab4fa1298c6c2a664b464
Claus-ginger <163654391+Claus-ginger@users.noreply.github.com>
2026-04-05 23:24:34 +0900

    update B24.py

--- tessoku/4_Dynamic_Programming/B24.py
+++ tessoku/4_Dynamic_Programming/B24.py
@@ -1,6 +1,20 @@
+# LIS ... 最長増加部分列(Longest Increasing Subsequence)
+
 import sys
 import bisect
 input = sys.stdin.readline
+
+def Get_LISvalue(A):
+    LEN = 0
+    L = []
+    for i in range(N):
+        pos = bisect.bisect_left(L, A[i])
+        if pos == LEN:
+            L.append(A[i])
+            LEN += 1
+        else:
+            L[pos] = A[i]
+    return LEN
 
 N = int(input())
 X = [None] * N
@@ -8,10 +22,15 @@ Y = [None] * N
 for i in range(N):
     X[i], Y[i] = map(int, input().split())
 
-LEN = 0
-L = []
-dp = [None] * N
-
+tmp = []
 for i in range(N):
-    W = bisect.bisect_left(L, X[i])
-    H = bisect.bisect_left(L, Y[i])
+    tmp.append([X[i], -Y[i]])
+    # -Yとする理由
+    # 昇順に並べると，Xが同じとき両方とってしまう．これはよくないので降順
+tmp.sort()
+
+A = []
+for i in range(N):
+    A.append(-tmp[i][1])
+
+print(Get_LISvalue(A))
